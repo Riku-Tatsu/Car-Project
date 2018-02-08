0d21c5fde529e0660d9a463d9da7d9ee5a334795
chillichips2 <36012781+chillichips2@users.noreply.github.com>
2018-02-08 21:03:06 +0100

    Revert "Revert "Can't discard these for some reason so here you go ?¿""
    This reverts commit 042b0f9a93cca1fd53672d346eb3aad5250e6d3d.

---
+++ Car physics multiplayer game/Assets/Scripts/Rendering/RenderingGlobalVariables.cs
@@ -0,0 +1,33 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class RenderingGlobalVariables : MonoBehaviour {
+
+	bool frameSign = true;
+
+	float frameTimer = 0;
+
+	void Start ()
+	{
+		frameTimer = 1f / Screen.currentResolution.refreshRate;
+	}
+	
+	
+	void LateUpdate ()
+	{
+		if(frameTimer > 0)
+		{
+			frameTimer -= Time.deltaTime;
+		}
+		else
+		{
+			frameTimer = 1f / Screen.currentResolution.refreshRate;
+
+			frameSign = !frameSign;
+
+			Shader.SetGlobalFloat("_FrameSign", frameSign ? 1 : -1);
+		}
+		
+	}
+}
